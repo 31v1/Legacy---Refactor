e22e458cb705d77fce10f44065d171b7b62fb842
Elvia Sanchez Leiva <rosio.sanchezleiva@gmail.com>
2026-03-15 01:35:36 -0600

    Update main.java

--- PowerBill/Refactor/java/main.java
+++ PowerBill/Refactor/java/main.java
@@ -5,7 +5,7 @@ package powerbill;
  * Creates the object and calls menu.
  */
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.run();
