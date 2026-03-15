892ec230cfa7ac4ec96d1885a8eba5bbd02d76af
Elvia Sanchez Leiva <rosio.sanchezleiva@gmail.com>
2026-03-14 23:37:58 -0600

    Update Validaciones.h

--- InventoryManagement/Refactor/Validaciones.h
+++ InventoryManagement/Refactor/Validaciones.h
@@ -1,1 +1,10 @@
+#ifndef VALIDACIONES_H
+#define VALIDACIONES_H
 
+#include <string>
+using namespace std;
+
+bool validarFecha(const string& fecha);
+// Aquí puedes agregar después otras validaciones (cédula, códigos, etc.)
+
+#endif
