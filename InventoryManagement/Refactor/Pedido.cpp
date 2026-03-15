9b23e7ac13d39504cac687b62e3b8c41e2018e67
Elvia Sanchez Leiva <rosio.sanchezleiva@gmail.com>
2026-03-14 23:37:33 -0600

    Update Pedido.cpp

--- InventoryManagement/Refactor/Pedido.cpp
+++ InventoryManagement/Refactor/Pedido.cpp
@@ -2,12 +2,8 @@
 #include <iostream>
 using namespace std;
 
-// Constructor por defecto
-Pedido::Pedido() {}
-
-// Constructor con parámetros
-Pedido::Pedido(string cod, string codPieza, int cant, string idEmpleado, 
-               string fechaSoli, string estad) {
+//constructores
+Pedido::Pedido(string cod, string codPieza, int cant, string idEmpleado, string fechaSoli, string estad) {
     codigo = cod;
     codigoPieza = codPieza;
     cantidad = cant;
@@ -16,29 +12,23 @@ Pedido::Pedido(string cod, string codPieza, int cant, string idEmpleado,
     estado = estad;
 }
 
-// Mostrar información
-void Pedido::mostrarInfo() const {
-    cout << "Código de pedido: " << codigo << endl;
-    cout << "Código de pieza: " << codigoPieza << endl;
-    cout << "Cantidad: " << cantidad << endl;
-    cout << "Cédula empleado: " << identificacionEmpleado << endl;
-    cout << "Fecha solicitud: " << fechaSolicitud << endl;
-    cout << "Estado: " << estado << endl;
-    cout << "------------------------" << endl;
-}
+Pedido::Pedido() {}
 
-// Getters
+// Métodos 'get' y 'set' para cada atributo
 string Pedido::getCodigo() const { return codigo; }
-string Pedido::getCodigoPieza() const { return codigoPieza; }
-int Pedido::getCantidad() const { return cantidad; }
-string Pedido::getIdentificacionEmpleado() const { return identificacionEmpleado; }
-string Pedido::getFechaSolicitud() const { return fechaSolicitud; }
-string Pedido::getEstado() const { return estado; }
-
-// Setters
 void Pedido::setCodigo(string cod) { codigo = cod; }
+
+string Pedido::getCodigoPieza() const { return codigoPieza; }
 void Pedido::setCodigoPieza(string codPieza) { codigoPieza = codPieza; }
+
+int Pedido::getCantidad() const { return cantidad; }
 void Pedido::setCantidad(int cant) { cantidad = cant; }
-void Pedido::setIdentificacionEmpleado(string idEmpleado) { identificacionEmpleado = idEmpleado; }
+
+string Pedido::getFechaSolicitud() const { return fechaSolicitud; }
 void Pedido::setFechaSolicitud(string fechaSoli) { fechaSolicitud = fechaSoli; }
+
+string Pedido::getIdentificacionEmpleado() const { return identificacionEmpleado; }
+void Pedido::setIdentificacionEmpleado(string idEmpleado) { identificacionEmpleado = idEmpleado; }
+
+string Pedido::getEstado() const { return estado; }
 void Pedido::setEstado(string estad) { estado = estad; }
