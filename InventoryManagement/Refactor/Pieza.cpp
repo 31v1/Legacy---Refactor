ab55ef9488a766a6eb7d7fc72d3a5825ede427e8
Elvia Sanchez Leiva <rosio.sanchezleiva@gmail.com>
2026-03-14 23:36:31 -0600

    Update Pieza.cpp

--- InventoryManagement/Refactor/Pieza.cpp
+++ InventoryManagement/Refactor/Pieza.cpp
@@ -2,12 +2,8 @@
 #include <iostream>
 using namespace std;
 
-// Constructor por defecto
-Pieza::Pieza() {}
-
-// Constructor con parámetros
-Pieza::Pieza(string cod, string nom, double cost, int cant, 
-             string emp, string fact, string ced) {
+//constructor con parametros
+Pieza::Pieza(string cod, string nom, double cost, int cant, string emp, string fact, string ced) {
     codigo = cod;
     nombre = nom;
     costo = cost;
@@ -17,32 +13,39 @@ Pieza::Pieza(string cod, string nom, double cost, int cant,
     cedula = ced;
 }
 
-// Mostrar información
-void Pieza::mostrarInfo() const {
-    cout << "Código: " << codigo << endl;
-    cout << "Nombre: " << nombre << endl;
-    cout << "Costo: " << costo << endl;
-    cout << "Cantidad: " << cantidad << endl;
-    cout << "Empresa: " << empresa << endl;
-    cout << "Factura: " << factura << endl;
-    cout << "Cédula quien ingresa: " << cedula << endl;
-    cout << "------------------------" << endl;
+//Constructor por defecto
+Pieza::Pieza() {}
+
+// mostrar informacion pieza
+void Pieza::mostrarInfo() {
+    cout << "Pieza con "
+         << "Código: " << codigo << ", "
+         << "Nombre: " << nombre << ", "
+         << "Costo: " << costo << ", "
+         << "Cantidad: " << cantidad << ", "
+         << "Empresa: " << empresa << ", "
+         << "Factura: " << factura << ", "
+         << "Cédula: " << cedula << ", " << endl;
 }
 
-// Getters
+// Métodos 'get' y 'set' para cada atributo
 string Pieza::getCodigo() const { return codigo; }
-string Pieza::getNombre() const { return nombre; }
-double Pieza::getCosto() const { return costo; }
-int Pieza::getCantidad() const { return cantidad; }
-string Pieza::getEmpresa() const { return empresa; }
-string Pieza::getFactura() const { return factura; }
-string Pieza::getCedula() const { return cedula; }
-
-// Setters
 void Pieza::setCodigo(string cod) { codigo = cod; }
+
+string Pieza::getNombre() const { return nombre; }
 void Pieza::setNombre(string nom) { nombre = nom; }
+
+double Pieza::getCosto() const { return costo; }
 void Pieza::setCosto(double cost) { costo = cost; }
+
+int Pieza::getCantidad() const { return cantidad; }
 void Pieza::setCantidad(int cant) { cantidad = cant; }
+
+string Pieza::getEmpresa() const { return empresa; }
 void Pieza::setEmpresa(string emp) { empresa = emp; }
+
+string Pieza::getFactura() const { return factura; }
 void Pieza::setFactura(string fact) { factura = fact; }
+
+string Pieza::getCedula() const { return cedula; }
 void Pieza::setCedula(string ced) { cedula = ced; }
